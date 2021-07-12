866c4e1b17034d740b0b3d48eaa8db5eb8ab41af
KentaroYoshizumi <schecter3.2.1-@softbank.ne.jp>
2021-07-13 08:00:45 +0900

    dnd complete

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,13 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - nuxt-every',
-    title: 'nuxt-every',
+    titleTemplate: '%s - every',
+    title: 'every',
     htmlAttrs: {
       lang: 'en'
     },
@@ -21,11 +24,16 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/style.scss'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/filters'
   ],
+  router: {
+    middleware: 'auth'
+  },
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -38,28 +46,42 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/pwa',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: "AIzaSyAXo-Q8wXE1HW9oq6UEkZYAH9RlgRTNJXs",
+          authDomain: "every-a45c5.firebaseapp.com",
+          projectId: "every-a45c5",
+          storageBucket: "every-a45c5.appspot.com",
+          messagingSenderId: "463893649972",
+          appId: "1:463893649972:web:0bc60df76f6240eb01c861",
+          measurementId: "G-79DMRM88CK"
+        },
+
+
+        services: {
+          auth: {
+            persistence: 'local', // default
+            initialize: {
+              onAuthStateChangedAction: 'onAuthStateChangedAction',
+              subscribeManually: false
+            },
+            ssr: false,
+          },
+          firestore: true,
+          storage: true
+        }
+      }
+    ]
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   }
 }
